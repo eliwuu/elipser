947a52dfe8dda89de3f6a8cfe6f4cfad9139508c
eliwuu <marekwilk@me.com>
2022-01-30 14:38:46 +0100

    initial tokenizer

--- src/tokenizer.ts
+++ src/tokenizer.ts
@@ -3,11 +3,49 @@ const punctuation = [",", ";", ":"];
 const quotation = ['"', "'"];
 const sentenceEnd = [".", "?", "!", "..."];
 
-const tokenize = (str: string) => {
+export type Tokenized = { word: string; len: number; idx: number };
+export type Tokenize = (str: string) => Tokenized[];
+
+export const truncate = (
+  str: string,
+  len: number,
+  options?: {
+    maxLen?: number;
+    preserveNumbers?: boolean;
+    preserveQuotation?: boolean;
+  }
+) => {
+  if (len <= str.length) return str;
+
+  if (options?.maxLen) {
+    if (len <= options.maxLen) return str;
+    if (str.length <= options.maxLen) return str;
+
+    str.slice(0, options.maxLen);
+  } else {
+    str.slice(0, len);
+  }
+  const tokenized = tokenize(str);
+  const count = spaceCount(str);
+  return str.slice(0, len);
+};
+
+export const truncateNoOptions = () => null;
+export const truncateMaxLenght = () => null;
+export const truncatePreferMaxLength = () => null;
+export const truncatePreserveQuotation = () => null;
+export const truncatePreserveNumbers = () => null;
+
+export const spaceCount = (str: string) =>
+  str.split("").filter((x) => x === " ").length;
+
+export const tokenize: Tokenize = (str: string) => {
   return str.split(" ").map((word, index) => {
-    return { word: word, idx: index };
+    return { word: word, len: word.length, idx: index };
   });
 };
+
+export const getTotalLength = (str: string) => tokenize;
 
 const isQuote = (word: string) => {
   const firstItem = quotation.map((x) => x === word[0])[0];
--- test/tokenizer.test.ts
+++ test/tokenizer.test.ts
@@ -1,3 +1,6 @@
+import { expect } from "chai";
+import { spaceCount, tokenize, truncate } from "../src/tokenizer";
+
 const noPunctuation = "Twoja stara pierze w rzece a twój stary czary mary";
 const dotEnd = "Twoja stara pierze w rzece a twój stary czary mary.";
 const twoDots = "Twoja stara pierze w rzece. Twój stary czary mary.";
@@ -5,7 +8,35 @@ const comma = "Twoja stara pierze w rzece, twój stary czary mary.";
 const quote = 'Twoja stara "pierze w rzece a twój stary" czary mary';
 
 describe("tokenizer test", () => {
+  it("str length equals tokenized length", () => {
+    const expected = noPunctuation.length;
+
+    const tokenized = tokenize(noPunctuation);
+
+    const result =
+      tokenized.map((x) => x.len).reduce((x, y) => x + y) +
+      spaceCount(noPunctuation);
+
+    expect(result).equal(expected);
+  });
   it("get tokens from string", () => {
     const result = "Twoja stara pierze";
+  });
+  it("truncate length", () => {
+    const resultLengthGreaterThanStrLen = truncate(noPunctuation, 40);
+
+    const expectedLenghtGreaterThanStrLen = noPunctuation.length;
+
+    expect(resultLengthGreaterThanStrLen.length).equal(
+      expectedLenghtGreaterThanStrLen
+    );
+  });
+  it("truncate maxLen", () => {
+    const resultMaxLenEqualStrLen = truncate(noPunctuation, 10, {
+      maxLen: noPunctuation.length,
+    });
+    const expectedLengthEqualStrLen = noPunctuation.length;
+
+    expect(resultMaxLenEqualStrLen.length).equal(expectedLengthEqualStrLen);
   });
 });
